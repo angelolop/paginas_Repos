f672e89b51d4159268fd07b984a1e6c57f9313ae
angelolop <angelo.lop.alv@gmail.com>
2022-04-07 09:22:11 -0300

    site_responsivo

---
+++ js/buttons.js
@@ -0,0 +1,7 @@
+//função dos botões do main//
+function botaoGithub(){
+    window.open("https://github.com/angelolop")
+};
+function botaoLinkedin(){
+    window.open("https://www.linkedin.com/in/angelo-/")
+};
---
+++ js/owl/main.js
@@ -0,0 +1,18 @@
+//configurações para o efeito carrossel da página//
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:8,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
